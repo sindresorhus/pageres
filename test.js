174b1db8eab4301c1e75de0c3ef2eb12e425b89b
Kevin Martensson <kevinmartensson@gmail.com>
2014-02-13 17:10:03 +0100

    Add screenshot test

--- test.js
+++ test.js
@@ -1,9 +1,25 @@
+/* global afterEach, beforeEach, it */
 'use strict';
 var assert = require('assert');
+var fs = require('fs');
 var pageres = require('./index');
+var rm = require('rimraf');
 
-//TODO !!
-it('should ', function (cb) {
-	assert(true);
+afterEach(function (cb) {
+	process.chdir('../');
+	rm('tmp', cb);
+});
+
+beforeEach(function (cb) {
+	fs.mkdirSync('tmp');
+	process.chdir('tmp');
 	cb();
+});
+
+it('should generate screenshots', function (cb) {
+	pageres(['yeoman.io'], ['1024x768', '640x480'], function () {
+		assert(fs.existsSync('yeoman.io-640x480.png'));
+		assert(fs.existsSync('yeoman.io-1024x768.png'));
+		cb();
+	});
 });
