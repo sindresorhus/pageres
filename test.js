983065ac7685ccd8c03fe3649879852c07811969
Sindre Sorhus <sindresorhus@gmail.com>
2014-02-13 18:12:37 +0100

    tests - make sure it also supports multiple urls

--- test.js
+++ test.js
@@ -1,9 +1,9 @@
-/* global afterEach, beforeEach, it */
+/*global afterEach, beforeEach, it */
 'use strict';
 var assert = require('assert');
 var fs = require('fs');
-var pageres = require('./index');
 var rm = require('rimraf');
+var pageres = require('./index');
 
 afterEach(function (cb) {
 	process.chdir('../');
@@ -17,9 +17,11 @@ beforeEach(function (cb) {
 });
 
 it('should generate screenshots', function (cb) {
-	pageres(['yeoman.io'], ['1024x768', '640x480'], function () {
+	pageres(['yeoman.io', 'todomvc.com'], ['1024x768', '640x480'], function () {
 		assert(fs.existsSync('yeoman.io-640x480.png'));
 		assert(fs.existsSync('yeoman.io-1024x768.png'));
+		assert(fs.existsSync('todomvc.com-640x480.png'));
+		assert(fs.existsSync('todomvc.com-1024x768.png'));
 		cb();
 	});
 });
