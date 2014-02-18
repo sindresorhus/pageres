74f491b55628f705dbb612c309d39660c3802d3b
Werner van Deventer <werner@brutaldev.com>
2014-02-18 18:53:00 +0200

    Split up unit test for special characters
    Split up the unit test for special characters so it's nice an isolated
    to just looking at the file name being generated.

--- test.js
+++ test.js
@@ -7,15 +7,26 @@ var pageres = require('./index');
 it('should generate screenshots', function (cb) {
 	this.timeout(20000);
 
-	pageres(['yeoman.io', 'todomvc.com', 'http://microsoft.com/?query=pageres*|<>:"\\'], ['1024x768', '640x480'], function (err, streams) {
+	pageres(['yeoman.io', 'todomvc.com'], ['1024x768', '640x480'], function (err, streams) {
 		assert(!err);
-		assert.strictEqual(streams.length, 6);
+		assert.strictEqual(streams.length, 4);
 		assert.strictEqual(streams[0].filename, 'yeoman.io-1024x768.png');
-		assert.strictEqual(streams[4].filename, 'microsoft.com!query=pageres-1024x768.png');
 
 		streams[0].once('data', function (data) {
 			assert(data.length > 1000);
 			cb();
 		});
+	});
+});
+
+it('should remove special characters from the URL to create a valid filename', function (cb) {
+	this.timeout(20000);
+
+	pageres(['http://microsoft.com/?query=pageres*|<>:"\\'], ['1024x768'], function (err, streams) {
+		assert(!err);
+		assert.strictEqual(streams.length, 1);
+		assert.strictEqual(streams[0].filename, 'microsoft.com!query=pageres-1024x768.png');
+		
+		cb();
 	});
 });
