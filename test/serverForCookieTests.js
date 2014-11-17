2e6902b4e2866eb079a481240bac502af1569c56
Kevin Martensson <kevinmartensson@gmail.com>
2014-11-17 15:22:30 +0100

    Simplify cookie test

--- test/serverForCookieTests.js
+++ test/serverForCookieTests.js
@@ -1,18 +1,15 @@
 'use strict';
-var express = require('express');
-var cookieParser = require('cookie-parser');
 var http = require('http');
-var app = express();
-
-app.use(cookieParser());
-app.get('/', function (req, res) {
-	var color = req.cookies.pageresColor || 'white';
-	res.type('html');
-	res.send('<body><div style="background: ' + color + ';position: absolute;top: 0;bottom: 0;left: 0;right: 0;"></div></body');
-});
+var cookie = require('cookie');
 
 module.exports = function (port) {
-	var server = http.createServer(app);
+	var server = http.createServer(function (req, res) {
+		var color = cookie.parse(req.headers.cookie).pageresColor || 'white';
+
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.end('<body><div style="background: ' + color + ';position: absolute;top: 0;bottom: 0;left: 0;right: 0;"></div></body');
+	});
+
 	server.listen(port);
 	return server;
 };
