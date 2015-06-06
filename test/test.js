e83143ed62c1f8c283c0a22825825b5311045b05
Kevin Martensson <kevinmartensson@gmail.com>
2015-06-06 12:26:16 +0200

    Add more CLI tests

--- test/test.js
+++ test/test.js
@@ -257,6 +257,51 @@ test('remove temporary files on cancel', function (t) {
 	}, 500);
 });
 
+test('show error if no url is specified', function (t) {
+	t.plan(1);
+
+	var cp = spawn('../cli.js', ['320x240'], {
+		stdio: [process.stdin, null, null]
+	});
+
+	cp.stderr.setEncoding('utf8');
+	cp.stderr.on('data', function (data) {
+		t.assert(/Specify a url/.test(data), data);
+	});
+});
+
+test('use 1366x768 as default resolution', function (t) {
+	t.plan(2);
+
+	var cp = spawn('../cli.js', ['yeoman.io'], {
+		stdio: [process.stdin, null, null]
+	});
+
+	cp.on('close', function () {
+		t.assert(fs.existsSync('yeoman.io-1366x768.png'));
+
+		fs.unlink('yeoman.io-1366x768.png', function (err) {
+			t.assert(!err, err);
+		});
+	});
+});
+
+test('generate screenshots using keywords', function (t) {
+	t.plan(2);
+
+	var cp = spawn('../cli.js', ['yeoman.io', 'iphone5s'], {
+		stdio: [process.stdin, null, null]
+	});
+
+	cp.on('close', function () {
+		t.assert(fs.existsSync('yeoman.io-320x568.png'));
+
+		fs.unlink('yeoman.io-320x568.png', function (err) {
+			t.assert(!err, err);
+		});
+	});
+});
+
 test('show help screen', function (t) {
 	t.plan(1);
 
