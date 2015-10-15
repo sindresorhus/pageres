62ed3dc92ac574a36457f3574f18cf997762531e
Sam Verschueren <sam.verschueren@gmail.com>
2015-10-15 12:54:42 +0200

    Asyncifiy tests

--- test/test.js
+++ test/test.js
@@ -4,8 +4,14 @@ import imageSize from 'image-size';
 import concatStream from 'concat-stream';
 import easydate from 'easydate';
 import PNG from 'png-js';
+import getStream from 'get-stream';
+import pify from 'pify';
+import rfpify from 'rfpify';
+import Promise from 'pinkie-promise';
 import Pageres from '../dist';
 import Server from './serverForCookieTests';
+
+const promiseFs = pify.all(fs, Promise);
 
 process.chdir(__dirname);
 
@@ -15,235 +21,178 @@ test('expose a constructor', t => {
 });
 
 test('add a source', t => {
-	const pageres = new Pageres()
-		.src('yeoman.io', ['1280x1024', '1920x1080']);
+	const pageres = new Pageres().src('yeoman.io', ['1280x1024', '1920x1080']);
 
 	t.is(pageres._src[0].url, 'yeoman.io');
 	t.end();
 });
 
 test('set destination directory', t => {
-	const pageres = new Pageres()
-		.dest('tmp');
+	const pageres = new Pageres().dest('tmp');
 
 	t.is(pageres._dest, 'tmp');
 	t.end();
 });
 
-test('error if no url is specified', t => {
-	t.plan(2);
-
-	new Pageres()
-		.src('', [])
-		.run()
-		.catch(err => {
-			t.ok(err);
-			t.is(err.message, 'URL required');
-		});
+test('error if no url is specified', async t => {
+	try {
+		await new Pageres().src('', []).run();
+		t.fail();
+	} catch (err) {
+		t.ok(err);
+		t.is(err.message, 'URL required');
+	}
 });
 
-test('generate screenshots', t => {
-	t.plan(4);
-
-	new Pageres()
+test('generate screenshots', async t => {
+	const streams = await new Pageres()
 		.src('yeoman.io', ['480x320', '1024x768', 'iphone 5s'])
 		.src('todomvc.com', ['1280x1024', '1920x1080'])
-		.run()
-		.then(streams => {
-			t.is(streams.length, 5);
-			t.is(streams[0].filename, 'todomvc.com-1280x1024.png');
-			t.is(streams[4].filename, 'yeoman.io-320x568.png');
-			streams[0].once('data', data => t.true(data.length > 1000));
-		});
+		.run();
+
+	t.is(streams.length, 5);
+	t.is(streams[0].filename, 'todomvc.com-1280x1024.png');
+	t.is(streams[4].filename, 'yeoman.io-320x568.png');
+
+	const data = await getStream.buffer(streams[0]);
+
+	t.true(data.length > 1000);
 });
 
-test('remove special characters from the URL to create a valid filename', t => {
-	t.plan(2);
+test('remove special characters from the URL to create a valid filename', async t => {
+	const streams = await new Pageres().src('http://www.microsoft.com/?query=pageres*|<>:"\\', ['1024x768']).run();
 
-	new Pageres()
-		.src('http://www.microsoft.com/?query=pageres*|<>:"\\', ['1024x768'])
-		.run()
-		.then(streams => {
-			t.is(streams.length, 1);
-			t.is(streams[0].filename, 'microsoft.com!query=pageres-1024x768.png');
-		});
+	t.is(streams.length, 1);
+	t.is(streams[0].filename, 'microsoft.com!query=pageres-1024x768.png');
 });
 
-test('have a `delay` option', t => {
-	t.plan(1);
+test('have a `delay` option', async t => {
+	const streams = await new Pageres({delay: 2}).src('http://todomvc.com', ['1024x768']).run();
 
-	new Pageres({delay: 2})
-		.src('http://todomvc.com', ['1024x768'])
-		.run()
-		.then(streams => {
-			const now = Date.now();
-			streams[0].once('data', () => t.true(Date.now() - now > 2000));
-		});
+	const now = Date.now();
+
+	await getStream(streams[0]);
+
+	t.true(Date.now() - now > 2000);
 });
 
-test('crop image using the `crop` option', t => {
-	t.plan(3);
+test('crop image using the `crop` option', async t => {
+	const streams = await new Pageres({ crop: true }).src('http://todomvc.com', ['1024x768']).run();
 
-	new Pageres({crop: true})
-		.src('http://todomvc.com', ['1024x768'])
-		.run()
-		.then(streams => {
-			t.is(streams[0].filename, 'todomvc.com-1024x768-cropped.png');
+	t.is(streams[0].filename, 'todomvc.com-1024x768-cropped.png');
 
-			streams[0].pipe(concatStream(data => {
-				const size = imageSize(data);
-				t.is(size.width, 1024);
-				t.is(size.height, 768);
-			}));
-		});
+	let data = await getStream.buffer(streams[0]);
+
+	const size = imageSize(data);
+	t.is(size.width, 1024);
+	t.is(size.height, 768);
 });
 
-test('rename image using the `filename` option', t => {
-	t.plan(2);
-
-	new Pageres()
+test('rename image using the `filename` option', async t => {
+	const streams = await new Pageres()
 		.src('http://todomvc.com', ['1024x768'], {
 			filename: '<%= date %> - <%= time %> - <%= url %>'
 		})
-		.run()
-		.then(streams => {
-			t.is(streams.length, 1);
-			t.regexTest(new RegExp(`${easydate('Y-M-d')} - \\d{2}-\\d{2}-\\d{2} - todomvc.com.png`), streams[0].filename);
-		});
+		.run();
+
+	t.is(streams.length, 1);
+	t.regexTest(new RegExp(`${easydate('Y-M-d')} - \\d{2}-\\d{2}-\\d{2} - todomvc.com.png`), streams[0].filename);
 });
 
-test('capture a DOM element using the `selector` option', t => {
-	t.plan(3);
+test('capture a DOM element using the `selector` option', async t => {
+	const streams = await new Pageres({selector: '.page-header'}).src('http://yeoman.io', ['1024x768']).run();
 
-	new Pageres({selector: '.page-header'})
-		.src('http://yeoman.io', ['1024x768'])
-		.run()
-		.then(streams => {
-			t.is(streams[0].filename, 'yeoman.io-1024x768.png');
+	t.is(streams[0].filename, 'yeoman.io-1024x768.png');
 
-			streams[0].pipe(concatStream(data => {
-				const size = imageSize(data);
-				t.is(size.width, 1024);
-				t.is(size.height, 80);
-			}));
-		});
+	const data = await getStream.buffer(streams[0]);
+
+	const size = imageSize(data);
+	t.is(size.width, 1024);
+	t.is(size.height, 80);
 });
 
-test('support local relative files', t => {
-	t.plan(2);
+test('support local relative files', async t => {
+	const streams = await new Pageres().src('fixture.html', ['1024x768']).run();
 
-	new Pageres()
-		.src('fixture.html', ['1024x768'])
-		.run()
-		.then(streams => {
-			t.is(streams[0].filename, 'fixture.html-1024x768.png');
-			streams[0].once('data', data => t.true(data.length > 1000));
-		});
+	t.is(streams[0].filename, 'fixture.html-1024x768.png');
+
+	const data = await getStream.buffer(streams[0]);
+
+	t.true(data.length > 1000);
 });
 
-test('fetch resolutions from w3counter', t => {
-	t.plan(2);
+test('fetch resolutions from w3counter', async t => {
+	const streams = await new Pageres().src('yeoman.io', ['w3counter']).run();
 
-	new Pageres()
-		.src('yeoman.io', ['w3counter'])
-		.run()
-		.then(streams => {
-			t.is(streams.length, 10);
-			streams[0].once('data', data => t.true(data.length > 1000));
-		});
+	t.is(streams.length, 10);
+
+	const data = await getStream.buffer(streams[0]);
+
+	t.true(data.length > 1000);
 });
 
-test('save image', t => {
-	t.plan(2);
+test('save image', async t => {
+	try {
+		await new Pageres().src('http://todomvc.com', ['1024x768']).dest(__dirname).run();
 
-	new Pageres()
-		.src('http://todomvc.com', ['1024x768'])
-		.dest(__dirname)
-		.run()
-		.then(() => {
-			t.true(fs.existsSync('todomvc.com-1024x768.png'));
-			fs.unlink('todomvc.com-1024x768.png', err => t.ifError(err));
-		});
+		t.true(fs.existsSync('todomvc.com-1024x768.png'));
+	} finally {
+		await promiseFs.unlink('todomvc.com-1024x768.png');
+	}
 });
 
-test('remove temporary files on error', t => {
-	t.plan(4);
+test('remove temporary files on error', async t => {
+	try {
+		await new Pageres().src('this-is-a-error-site.io', ['1024x768']).dest(__dirname).run();
+	} catch (err) {
+		t.ok(err);
+		t.is(err.message, 'Couldn\'t load url: http://this-is-a-error-site.io');
 
-	new Pageres()
-		.src('this-is-a-error-site.io', ['1024x768'])
-		.dest(__dirname)
-		.run()
-		.catch(err => {
-			t.ok(err);
-			t.is(err.message, 'Couldn\'t load url: http://this-is-a-error-site.io');
+		const files = await promiseFs.readdir(__dirname);
 
-			fs.readdir(__dirname, (err, files) => {
-				t.ifError(err);
-				t.is(files.indexOf('this-is-a-error-site.io.png'), -1);
-			});
-		});
+		t.is(files.indexOf('this-is-a-error-site.io.png'), -1);
+	}
 });
 
-test('auth using username and password', t => {
-	t.plan(2);
+test('auth using username and password', async t => {
+	const streams = await new Pageres({username: 'user', password: 'passwd'}).src('httpbin.org/basic-auth/user/passwd', ['120x120']).run()
 
-	new Pageres({
-		username: 'user',
-		password: 'passwd'
-	})
-		.src('httpbin.org/basic-auth/user/passwd', ['120x120'])
-		.run()
-		.then(streams => {
-			t.is(streams.length, 1);
-			streams[0].once('data', data => t.ok(data.length));
-		});
+	t.is(streams.length, 1);
+
+	const data = await getStream.buffer(streams[0]);
+
+	t.ok(data.length);
 });
 
-test('scale webpage using the `scale` option', t => {
-	t.plan(2);
+test('scale webpage using the `scale` option', async t => {
+	const streams = await new Pageres({scale: 2, crop: true}).src('yeoman.io', ['120x120']).run();
 
-	new Pageres({
-		scale: 2,
-		crop: true
-	})
-		.src('yeoman.io', ['120x120'])
-		.run()
-		.then(streams => {
-			streams[0].pipe(concatStream(data => {
-				const size = imageSize(data);
-				t.is(size.width, 240);
-				t.is(size.height, 240);
-			}));
-		});
+	const data = await getStream.buffer(streams[0]);
+
+	const size = imageSize(data);
+	t.is(size.width, 240);
+	t.is(size.height, 240);
 });
 
-function cookieTest(port, input, t) {
-	t.plan(5);
-
+async function cookieTest(port, input, t) {
 	const server = new Server(port);
 	const filename = `localhost!${port}-320x480.png`;
 
-	new Pageres({
-		cookies: [input]
-	})
-		.src(`http://localhost:${port}`, ['320x480'])
-		.run()
-		.then(streams => {
-			t.is(streams[0].filename, filename);
+	const streams = await new Pageres({cookies: [input]}).src(`http://localhost:${port}`, ['320x480']).run()
 
-			streams[0].pipe(concatStream(data => {
-				server.close();
+	t.is(streams[0].filename, filename);
 
-				const png = new PNG(data);
+	const data = await getStream.buffer(streams[0]);
 
-				png.decode(pixels => {
-					t.is(pixels[0], 0);
-					t.is(pixels[1], 0);
-					t.is(pixels[2], 0);
-					t.is(pixels[3], 255);
-				});
-			}));
-		});
+	server.close();
+
+	const png = new PNG(data);
+	const pixels = await rfpify(png.decode.bind(png), Promise)();
+
+	t.is(pixels[0], 0);
+	t.is(pixels[1], 0);
+	t.is(pixels[2], 0);
+	t.is(pixels[3], 255);
 }
 
 test('send cookie', cookieTest.bind(null, 5000, 'pageresColor=black; Path=/; Domain=localhost'));
